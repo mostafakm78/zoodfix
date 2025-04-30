fb08b0f82641abde5a1d7530e56243db2e4b3a16
Mostafa Kamari <mostafamf555@gmail.com>
2025-04-30 20:55:44 +0330

    add animaton fade to footer

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -1,6 +1,9 @@
+'use client';
+
 import Image from 'next/image';
 import Link from 'next/link';
 import { FaFacebook, FaFacebookMessenger, FaInstagram, FaTelegram, FaWhatsapp } from 'react-icons/fa';
+import { motion } from 'framer-motion';
 
 // photos
 import Phone from '../../../public/images/phone-ft.png';
@@ -11,108 +14,112 @@ export const Footer: React.FC = () => {
     // background for footer
     <div className="md:bg-[url('/images/footer.png')] md:bg-transparent bg-secondery bg-cover lg:px-48 px-12 py-20 lg:justify-items-end grid grid-cols-1 lg:grid-cols-2">
       {/* right section  */}
-      <div className="flex flex-col lg:mt-44 items-center lg:items-start md:mt-64">
-        <h3 className="text-2xl text-amber-200 mb-3">درباره زود فیکس</h3>
-        <p className="text-base/relaxed lg:w-10/12 text-justify text-background mb-6">
-          دانش و تجربه 30 ساله ما را بر آن داشت تا فعالیتی نوین در راستای بهبود ساختمان سازی برداریم که از سال 95 طراحی و ساخت کارخانه زودفیکس استارت خورد و یک کارخانه تمام ایرانی و اتومات با دانش فنی داخلی که در نوع خود بی نظیر می باشد ساخته و با تنوع و بسته بندی حدود 30 نوع ملات و چسب های نوین
-          ساختمانی در انواع بسته بندی های از 500 گرم تا 50 کیلو گرم براساس استانداردهای ملی و نیاز ساختمان سازی تولید نماییم
-        </p>
-        <div className="xl:flex xl:flex-row flex-col justify-between w-10/12">
-          <div className="flex">
-            <Image src={Phone} alt="call number" />
-            <div className="flex flex-col text-background mr-2">
-              <span>پشتیبانی سریع</span>
-              <span>09169799533</span>
+      <motion.div initial={{ opacity: 0, x: 80 }} whileInView={{ opacity: 1, x: 0 }} transition={{ duration: 1, delay: 0.3 }}>
+        <div className="flex flex-col lg:mt-44 items-center lg:items-start md:mt-64">
+          <h3 className="text-2xl text-amber-200 mb-3">درباره زود فیکس</h3>
+          <p className="text-base/relaxed lg:w-10/12 text-justify text-background mb-6">
+            دانش و تجربه 30 ساله ما را بر آن داشت تا فعالیتی نوین در راستای بهبود ساختمان سازی برداریم که از سال 95 طراحی و ساخت کارخانه زودفیکس استارت خورد و یک کارخانه تمام ایرانی و اتومات با دانش فنی داخلی که در نوع خود بی نظیر می باشد ساخته و با تنوع و بسته بندی حدود 30 نوع ملات و چسب های نوین
+            ساختمانی در انواع بسته بندی های از 500 گرم تا 50 کیلو گرم براساس استانداردهای ملی و نیاز ساختمان سازی تولید نماییم
+          </p>
+          <div className="xl:flex xl:flex-row flex-col justify-between w-10/12">
+            <div className="flex">
+              <Image src={Phone} alt="call number" />
+              <div className="flex flex-col text-background mr-2">
+                <span>پشتیبانی سریع</span>
+                <span>09169799533</span>
+              </div>
+            </div>
+            <div className="flex mt-4 xl:mt-0">
+              <Image src={Work} alt="call number" />
+              <div className="flex flex-col text-background mr-2">
+                <span>ساعت کاری</span>
+                <span>9:00 الی 19:00</span>
+              </div>
             </div>
           </div>
-          <div className="flex mt-4 xl:mt-0">
-            <Image src={Work} alt="call number" />
-            <div className="flex flex-col text-background mr-2">
-              <span>ساعت کاری</span>
-              <span>9:00 الی 19:00</span>
-            </div>
+          <div className="flex gap-4 text-4xl mb-6 text-yellow-200 mt-4">
+            <Link href="" className="hover:scale-110 duration-500">
+              <FaFacebook />
+            </Link>
+            <Link href="" className="hover:scale-110 duration-500">
+              <FaWhatsapp />
+            </Link>
+            <Link href="" className="hover:scale-110 duration-500">
+              <FaTelegram />
+            </Link>
+            <Link href="" className="hover:scale-110 duration-500">
+              <FaInstagram />
+            </Link>
+            <Link href="" className="hover:scale-110 duration-500">
+              <FaFacebookMessenger />
+            </Link>
+          </div>
+          <div className="group inline-block relative overflow-hidden text-background cursor-pointer">
+            {/* خط چپ */}
+            <div className="absolute left-0 bottom-0 w-[2px] h-0 bg-gray-700 transition-all duration-500 group-hover:h-full" />
+
+            {/* خط راست */}
+            <div className="absolute right-0 top-0 w-[2px] h-0 bg-gray-700 transition-all duration-500 group-hover:h-full" />
+
+            <Link target="_blank" href="https://portfolio-immostafakamari.vercel.app/" className="font-medium p-2 hover:text-gray-700 duration-500 relative inline-block">
+              {/* خط پایین */}
+              <span className="absolute bottom-0 right-0 h-[2px] w-0 bg-gray-700 transition-all duration-500 group-hover:w-full" />
+              {/* خط بالا */}
+              <span className="absolute top-0 left-0 h-[2px] w-0 bg-gray-700 transition-all duration-500 group-hover:w-full" />
+              بازطراحی شده توسط مصطفی کمری
+            </Link>
           </div>
         </div>
-        <div className="flex gap-4 text-4xl mb-6 text-yellow-200 mt-4">
-          <Link href="" className="hover:scale-110 duration-500">
-            <FaFacebook />
-          </Link>
-          <Link href="" className="hover:scale-110 duration-500">
-            <FaWhatsapp />
-          </Link>
-          <Link href="" className="hover:scale-110 duration-500">
-            <FaTelegram />
-          </Link>
-          <Link href="" className="hover:scale-110 duration-500">
-            <FaInstagram />
-          </Link>
-          <Link href="" className="hover:scale-110 duration-500">
-            <FaFacebookMessenger />
-          </Link>
-        </div>
-        <div className="group inline-block relative overflow-hidden text-background cursor-pointer">
-          {/* خط چپ */}
-          <div className="absolute left-0 bottom-0 w-[2px] h-0 bg-gray-700 transition-all duration-500 group-hover:h-full" />
-
-          {/* خط راست */}
-          <div className="absolute right-0 top-0 w-[2px] h-0 bg-gray-700 transition-all duration-500 group-hover:h-full" />
-
-          <Link target="_blank" href="https://portfolio-immostafakamari.vercel.app/" className="font-medium p-2 hover:text-gray-700 duration-500 relative inline-block">
-            {/* خط پایین */}
-            <span className="absolute bottom-0 right-0 h-[2px] w-0 bg-gray-700 transition-all duration-500 group-hover:w-full" />
-            {/* خط بالا */}
-            <span className="absolute top-0 left-0 h-[2px] w-0 bg-gray-700 transition-all duration-500 group-hover:w-full" />
-            بازطراحی شده توسط مصطفی کمری
-          </Link>
-        </div>
-      </div>
+      </motion.div>
 
       {/* left section  */}
-      <div className="flex flex-col lg:mt-44 mt-16">
-        <h4 className="text-2xl mb-4 text-amber-200">دسترسی سریع</h4>
-        <div className="flex gap-6 text-base/relaxed">
-          <div className='space-y-2'>
-            <Link href="/" className="flex hover:scale-110 duration-500 items-center">
-              <span className="font-semibold mb-1 text-amber-200">ـــ</span>
-              <span className="mr-2 text-background">خانه</span>
-            </Link>
-            <Link href="/products?page=1" className="flex hover:scale-110 duration-500 items-center">
-              <span className="font-semibold mb-1 text-amber-200">ـــ</span>
-              <span className="mr-2 text-background">محصولات ما</span>
-            </Link>
-            <Link href='/products?page=1&category=ملات خشک' className="flex hover:scale-110 duration-500 items-center">
-              <span className="font-semibold mb-1 text-amber-200">ـــ</span>
-              <span className="mr-2 text-background">ملات خشک</span>
-            </Link>
-            <Link href='/products?page=1&category=محصولات فروشگاهی' className="flex hover:scale-110 duration-500 items-center">
-              <span className="font-semibold mb-1 text-amber-200">ـــ</span>
-              <span className="mr-2 text-background">محصولات فروشگاهی</span>
-            </Link>
-            <Link href="/agents" className="flex hover:scale-110 duration-500 items-center">
-              <span className="font-semibold mb-1 text-amber-200">ـــ</span>
-              <span className="mr-2 text-background">اخذ نمایندگی</span>
-            </Link>
-          </div>
-          <div className='space-y-2'>
-            <Link href="/questions" className="flex hover:scale-110 duration-500 items-center">
-              <span className="font-semibold mb-1 text-amber-200">ـــ</span>
-              <span className="mr-2 text-background">سوالات متداول</span>
-            </Link>
-            <Link href="/about-us" className="flex hover:scale-110 duration-500 items-center">
-              <span className="font-semibold mb-1 text-amber-200">ـــ</span>
-              <span className="mr-2 text-background">درباره زودفیکس</span>
-            </Link>
-            <Link href="/contact-us" className="flex hover:scale-110 duration-500 items-center">
-              <span className="font-semibold mb-1 text-amber-200">ـــ</span>
-              <span className="mr-2 text-background">ارتباط با ما</span>
-            </Link>
-            <Link href="/employment" className="flex hover:scale-110 duration-500 items-center">
-              <span className="font-semibold mb-1 text-amber-200">ـــ</span>
-              <span className="mr-2 text-background">فرم استخدام</span>
-            </Link>
+      <motion.div initial={{ opacity: 0, x: -80 }} whileInView={{ opacity: 1, x: 0 }} transition={{ duration: 1, delay: 0.3 }}>
+        <div className="flex flex-col lg:mt-44 mt-16">
+          <h4 className="text-2xl mb-4 text-amber-200">دسترسی سریع</h4>
+          <div className="flex gap-6 text-base/relaxed">
+            <div className="space-y-2">
+              <Link href="/" className="flex hover:scale-110 duration-500 items-center">
+                <span className="font-semibold mb-1 text-amber-200">ـــ</span>
+                <span className="mr-2 text-background">خانه</span>
+              </Link>
+              <Link href="/products?page=1" className="flex hover:scale-110 duration-500 items-center">
+                <span className="font-semibold mb-1 text-amber-200">ـــ</span>
+                <span className="mr-2 text-background">محصولات ما</span>
+              </Link>
+              <Link href="/products?page=1&category=ملات خشک" className="flex hover:scale-110 duration-500 items-center">
+                <span className="font-semibold mb-1 text-amber-200">ـــ</span>
+                <span className="mr-2 text-background">ملات خشک</span>
+              </Link>
+              <Link href="/products?page=1&category=محصولات فروشگاهی" className="flex hover:scale-110 duration-500 items-center">
+                <span className="font-semibold mb-1 text-amber-200">ـــ</span>
+                <span className="mr-2 text-background">محصولات فروشگاهی</span>
+              </Link>
+              <Link href="/agents" className="flex hover:scale-110 duration-500 items-center">
+                <span className="font-semibold mb-1 text-amber-200">ـــ</span>
+                <span className="mr-2 text-background">اخذ نمایندگی</span>
+              </Link>
+            </div>
+            <div className="space-y-2">
+              <Link href="/questions" className="flex hover:scale-110 duration-500 items-center">
+                <span className="font-semibold mb-1 text-amber-200">ـــ</span>
+                <span className="mr-2 text-background">سوالات متداول</span>
+              </Link>
+              <Link href="/about-us" className="flex hover:scale-110 duration-500 items-center">
+                <span className="font-semibold mb-1 text-amber-200">ـــ</span>
+                <span className="mr-2 text-background">درباره زودفیکس</span>
+              </Link>
+              <Link href="/contact-us" className="flex hover:scale-110 duration-500 items-center">
+                <span className="font-semibold mb-1 text-amber-200">ـــ</span>
+                <span className="mr-2 text-background">ارتباط با ما</span>
+              </Link>
+              <Link href="/employment" className="flex hover:scale-110 duration-500 items-center">
+                <span className="font-semibold mb-1 text-amber-200">ـــ</span>
+                <span className="mr-2 text-background">فرم استخدام</span>
+              </Link>
+            </div>
           </div>
         </div>
-      </div>
+      </motion.div>
     </div>
   );
 };
